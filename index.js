8375af9829a29cfab305a3bd6ad838c90786a17a
joshuadsmith <joshua.d.smith@maine.edu>
2024-03-09 08:15:14 -0500

    added export

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import getFirebaseUserId from './lib/get-firebase-user-id.js';
+import getFirebaseUserEmail from './lib/get-firebase-user-email.js';
 
 export  {
     getFirebaseUserId,
+    getFirebaseUserEmail,
 };
 
 export default {
     getFirebaseUserId,
+    getFirebaseUserEmail,
 };
